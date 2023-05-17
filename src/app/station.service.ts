a5f6257139db91b17be3032d97bc95bc7ddfe6a7
gittjar <jarno.keranen@gmail.com>
2023-05-17 18:25:56 +0300

    modified more station.service and data flows now ok to component

--- src/app/station.service.ts
+++ src/app/station.service.ts
@@ -1,6 +1,5 @@
 import { Injectable, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +9,9 @@ export class StationService {
   constructor(private http: HttpClient) { }
 
   //get all stations from localhost
-  public getStations(): Observable<any> {
-    const BASEURL = '/stations';
-    return this.http.get<any>(BASEURL);
+  public getStations():any {
+    const stations = this.http.get('/stations');
+    return stations;
   }
 
 
