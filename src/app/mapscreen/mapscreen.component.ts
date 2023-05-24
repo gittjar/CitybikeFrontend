a272336089d7c52501a7b6b5301477404844a2b5
gittjar <jarno.keranen@gmail.com>
2023-05-24 09:38:22 +0300

    googlemaps added

--- src/app/mapscreen/mapscreen.component.ts
+++ src/app/mapscreen/mapscreen.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { StationService } from '../station.service';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-mapscreen',
@@ -9,8 +12,15 @@ import { StationService } from '../station.service';
 export class MapscreenComponent implements OnInit {
 
   stations : any
+  apiLoaded: Observable<boolean>;
 
-  constructor (private hpservice: StationService) {}
+  constructor (private hpservice: StationService, httpClient: HttpClient ) {
+    this.apiLoaded = httpClient.jsonp('https://maps.googleapis.com/maps/api/js?key=AIzaSyCPGsdM1rbk_01lcFMT2NvoKUbyyJh2Fhg', 'callback')
+    .pipe(
+      map(() => true),
+      catchError(() => of(false)),
+    );
+  }
 
   ngOnInit(): void {
     this.getAllStations();
@@ -20,5 +30,7 @@ export class MapscreenComponent implements OnInit {
     this.hpservice.getStations().subscribe((data: any) =>
     this.stations = data)
   }
+
+  
 
 }
